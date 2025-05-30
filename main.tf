8409e0630868b35d212a8b4eb4154206d547caf4
osei_tutujr <richardoseitutujnr@gmail.com>
2025-05-30 10:06:38 +0000

    Added files for CI/CD pipeline

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region = "us-east-1"
+}
+ 
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "room-3-bucket-1234567890" # Ensure this bucket name is globally unique
+  acl    = "private"
+}
