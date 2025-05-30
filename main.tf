1789fdb43703ffedca1280c4e0baba763d5f48c0
osei_tutujr <richardoseitutujnr@gmail.com>
2025-05-30 14:22:17 +0000

    Added files for CI/CD pipeline

--- main.tf
+++ main.tf
@@ -4,4 +4,10 @@ provider "aws" {
  
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "room-3-bucket-1234567890" # Ensure this bucket name is globally unique
+  force_destroy = true
+  tags = {
+    Name        = "Room 3 Bucket"
+    Environment = "Development"
+  }
+
 }
