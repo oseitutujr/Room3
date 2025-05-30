470cfa2f10757e9e98d39a57b8f9f2648fe6b50b
osei_tutujr <113070561+oseitutujr@users.noreply.github.com>
2025-05-30 21:07:38 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,119 @@
+
 provider "aws" {
   region = "us-east-1"
 }
- 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "room-3-bucket-1234567890" # Ensure this bucket name is globally unique
-  force_destroy = true
-  tags = {
-    Name        = "Room 3 Bucket"
-    Environment = "Development"
+
+variable "github_token" {}
+variable "conformity_api_key" {}
+
+resource "aws_s3_bucket" "artifact_bucket" {
+  bucket = "room-3-bucket-1234567890"
+}
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild-room3-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codebuild.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_policy" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_codebuild_project" "template_scanner" {
+  name          = "room3-template-scanner"
+  service_role  = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
   }
 
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:5.0"
+    type                        = "LINUX_CONTAINER"
+    environment_variable {
+      name  = "API_KEY"
+      value = var.conformity_api_key
+    }
+  }
+
+  source {
+    type      = "GITHUB"
+    location  = "https://github.com/oseitutujr/Room3"
+    buildspec = "buildspec.yml"
+  }
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = "codepipeline-room3-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codepipeline.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline_policy" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_codepipeline" "room3_pipeline" {
+  name     = "room3-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.artifact_bucket.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+    action {
+      name             = "GitHub_Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        Owner      = "oseitutujr"
+        Repo       = "Room3"
+        Branch     = "main"
+        OAuthToken = var.github_token
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+    action {
+      name             = "RunTemplateScanner"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      version          = "1"
+      configuration = {
+        ProjectName = aws_codebuild_project.template_scanner.name
+      }
+    }
+  }
 }
